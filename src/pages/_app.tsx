1885370c41a589d5933d1f1bb3c156783536ee63
antalmate12 <30597152+antalmate12@users.noreply.github.com>
2023-04-10 13:04:02 +0200

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+import { Analytics } from "@vercel/analytics/react";
+import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+
+export const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={roboto.className}>
+      <Analytics />
+      <Component {...pageProps} />
+    </div>
+  );
 }
