7f75c3aa937eb1aca3681103424a19df67d07f9f
antalmate12 <30597152+antalmate12@users.noreply.github.com>
2023-05-09 14:55:16 +0200

    app-dir migration 1

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import SuggestCard from "@/components/SuggestCard";
+import WhatIsThis from "@/components/WhatIsThis";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Hero />
+      <WhatIsThis />
+      <SuggestCard />
+    </main>
+  );
+}
